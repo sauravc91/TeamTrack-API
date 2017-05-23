d0389c4249e8acdb94b889dc408e17633ee995d9
Saurav Chatterjee <saurav.chatterjee@emids.com>
2017-05-23 17:28:13 +0530

    Changed routes

--- app/routes.js
+++ app/routes.js
@@ -68,24 +68,24 @@ module.exports = function (app) {
     });
 	
 	//Router Middle-ware for Authentication
-    // app.use(function(req, res, next) {
-        // var token = req.body.token || req.headers['x-access-token'];
-        // if (token) {
-            // jwt.verify(token, 'imsaurav', function(err, decoded) {          
-            // if (err) {
-                // return res.json({ success: false, message: 'Failed to authenticate token.' });      
-            // } else {
-                // req.decoded = decoded;  
-                // next();
-            // }
-            // });
-        // } else {
-            // return res.status(403).send({ 
-            // success: false, 
-            // message: 'No Authentication token provided.'
-            // });
-        // }
-    // });
+    app.use(function(req, res, next) {
+        var token = req.body.token || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, 'imsaurav', function(err, decoded) {          
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });      
+            } else {
+                req.decoded = decoded;  
+                next();
+            }
+            });
+        } else {
+            return res.status(403).send({ 
+            success: false, 
+            message: 'No Authentication token provided.'
+            });
+        }
+    });
 
 	//API call to create user
     app.post('/api/createUser', function (req, res) {
